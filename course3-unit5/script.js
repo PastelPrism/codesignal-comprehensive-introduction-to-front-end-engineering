bd1bdbb5503fc09a8e995c5728dc9603514895dd
PastelPrism <amy-van-leeuwen@proton.me>
2025-11-07 02:16:27 +0100

    Made a start with Unit 5

---
+++ course3-unit5/script.js
@@ -0,0 +1,23 @@
+// Assignment 3
+let moons = [
+    { name: "Europa", planet: "Jupiter", type: "icy" },  
+    { name: "Titan", planet: "Saturn", type: "rocky" }
+];
+
+document.getElementById("moonInfo").innerHTML =
+    "1. " + moons[0].name + " orbits " + moons[0].planet + " and is an " + moons[0].type + " moon<br>" +
+    "2. " + moons[1].name + " orbits " + moons[1].planet + " and is a " + moons[1].type + " moon";
+
+// Assignment 4
+
+
+// Assignment 5
+
+
+// Assignment 6
+
+
+// Assignment 7 
+
+
+// Assignment 8
